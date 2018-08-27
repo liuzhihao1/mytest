2fccca22de967d4e689b5024564255b45650c7b3
lzh <18702067532@163.com>
2018-08-27 17:31:03 +0800

    项目第一次提交

---
+++ git/src/main/java/demo.java
@@ -0,0 +1,10 @@
+/**
+ * @author google
+ * @description PACKAGE_NAME
+ * @date 2018/8/27
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("111111");
+    }
+}
